1b62f4504271e25f8846a368efa5ce0d9bf9f15f
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-22 15:26:27 +0800

    [ParkingLot][Story3] feat,case 3 should_return_two_cars_when_fetch_given_a_parking_lot

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -29,4 +29,20 @@ public class ParkingBoyTest {
         //Then
         assertEquals(car, fetchedCar);
     }
+
+    @Test
+    void should_return_two_cars_when_fetch_given_a_parking_lot() throws Exception {
+        //Given
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car car = new Car();
+        Ticket ticket = parkingBoy.park(car);
+        Car car2 = new Car();
+        Ticket ticket2 = parkingBoy.park(car2);
+        //When
+        Car fetchedCar = parkingBoy.fetch(ticket);
+        Car fetchedCar2 = parkingBoy.fetch(ticket2);
+        //Then
+        assertEquals(car, fetchedCar);
+        assertEquals(car2, fetchedCar2);
+    }
 }
