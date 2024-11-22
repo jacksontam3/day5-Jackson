5209022eebc461e4c19fe156173f891934c732e6
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-22 13:42:28 +0800

    [ParkingLot][Story1] feat, Case 3, return two car when fetch given two tickets

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -32,6 +32,22 @@ public class ParkingLotTest {
         assertEquals(car, fetchedCar);
     }
 
+    @Test
+    void should_return_two_cars_when_fetch_given_two_tickets() {
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket1 =  parkingLot.park(car1);
+        Ticket ticket2 =  parkingLot.park(car2);
+        //When
+        Car fetchedCar1 = parkingLot.fetch(ticket1);
+        Car fetchedCar2 = parkingLot.fetch(ticket2);
+        //Then
+        assertEquals(car1, fetchedCar1);
+        assertEquals(car2, fetchedCar2);
+    }
+
 
 
 }
