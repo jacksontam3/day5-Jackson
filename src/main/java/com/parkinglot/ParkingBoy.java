8808cfd5f2e91d9311d17b2392dac46de7ecd471
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-22 15:24:33 +0800

    [ParkingLot][Story3] feat,case 2 should_return_car_when_fetch_a_car_given_a_parking_lot

--- src/main/java/com/parkinglot/ParkingBoy.java
+++ src/main/java/com/parkinglot/ParkingBoy.java
@@ -7,4 +7,8 @@ public class ParkingBoy {
     public Ticket park(Car car){
         return parkingLot.park(car);
     }
+
+    public Car fetch(Ticket ticket) throws Exception {
+        return parkingLot.fetch(ticket);
+    }
 }
