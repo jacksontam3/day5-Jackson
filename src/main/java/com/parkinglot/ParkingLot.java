d0be50d8f5fd510bbcd0253fcf377fd942be9431
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-22 11:59:08 +0800

    [ParkingLot][Story1] feat, Case 2, return the car when fetch given a ticket

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,9 +1,27 @@
 package com.parkinglot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
 
+    private Map<Ticket, Car> ticketToCar = new HashMap<>();
 
     public Ticket park(Car car) {
-        return new Ticket();
+        Ticket ticket = new Ticket();
+        ticketToCar.put(ticket, car);
+        return ticket;
+    }
+
+    public Car fetch(Ticket ticket) {
+        return ticketToCar.get(ticket);
+    }
+
+    public Map<Ticket, Car> getTicketToCar() {
+        return ticketToCar;
+    }
+
+    public void setTicketToCar(Map<Ticket, Car> ticketToCar) {
+        this.ticketToCar = ticketToCar;
     }
 }
