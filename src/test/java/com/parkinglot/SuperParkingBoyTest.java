de0109b852204902562d830972a94424b791b4a2
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-23 13:47:59 +0800

    [ParkingLot][Story6] test [superparkingboy] , should_fetch_the_correct_car_with_each_ticket_when_fetching_given_two_parking_lot

--- src/test/java/com/parkinglot/SuperParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperParkingBoyTest.java
@@ -45,5 +45,23 @@ public class SuperParkingBoyTest {
         assertEquals(car, parkingLot2.fetch(ticket));
     }
 
+    @Test
+    void should_fetch_the_correct_car_with_each_ticket_when_fetching_given_two_parking_lot() throws Exception {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(10);
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        SuperParkingBoy superSmartParkingBoy = new SuperParkingBoy(Arrays.asList(parkingLot1, parkingLot2));
+
+        // When
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket1 = superSmartParkingBoy.park(car1);
+        Ticket ticket2 = superSmartParkingBoy.park(car2);
+
+        // Then
+        assertEquals(car1, superSmartParkingBoy.fetch(ticket1));
+        assertEquals(car2, superSmartParkingBoy.fetch(ticket2));
+    }
+
 
 }
