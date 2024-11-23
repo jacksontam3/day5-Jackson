b26988f32592b79f940cac56be4fd71ff5597ed3
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-24 00:08:37 +0800

    [ParkingLot][Story7] feat: let parking boy to learn parking strategy

--- src/main/java/com/parkinglot/ParkingBoy.java
+++ src/main/java/com/parkinglot/ParkingBoy.java
@@ -4,32 +4,28 @@ import java.util.List;
 
 public class ParkingBoy {
 
+    public static final String PARKING_LOTS_CANNOT_BE_NULL_OR_EMPTY = "Parking lots cannot be null or empty.";
     protected List<ParkingLot> parkingLots;
+    private final ParkingStrategy parkingStrategy;
 
-    public ParkingBoy(List<ParkingLot> parkingLots) {
+    public ParkingBoy(List<ParkingLot> parkingLots, ParkingStrategy parkingStrategy) {
         if (parkingLots == null || parkingLots.isEmpty()) {
-            throw new IllegalArgumentException("Parking lots cannot be null or empty.");
+            throw new IllegalArgumentException(PARKING_LOTS_CANNOT_BE_NULL_OR_EMPTY);
         }
         this.parkingLots = parkingLots;
+        this.parkingStrategy = parkingStrategy;
     }
 
     public Ticket park(Car car) {
-        for (ParkingLot parkingLot : parkingLots) {
-            if (parkingLot.hasAvailablePosition()) {
-                return parkingLot.park(car);
-            }
-        }
-        throw new NoAvailablePositionException();
+        ParkingLot selectedParkingLot = parkingStrategy.selectParkingLot(parkingLots);
+        return selectedParkingLot.park(car);
     }
 
     public Car fetch(Ticket ticket) throws Exception {
-        for (ParkingLot parkingLot : parkingLots) {
-            try {
-                return parkingLot.fetch(ticket);
-            } catch (UnrecognizedParkingTickerException e) {
-
-            }
-        }
-        throw new UnrecognizedParkingTickerException();
+        return parkingLots.stream()
+                .filter(parkingLot -> parkingLot.hasTicket(ticket))
+                .findFirst()
+                .orElseThrow(UnrecognizedParkingTickerException::new)
+                .fetch(ticket);
     }
 }
