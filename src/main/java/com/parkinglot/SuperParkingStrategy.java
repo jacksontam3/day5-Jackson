84ea554bfd3236162d276f2146d9b25eae208671
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-23 23:56:09 +0800

    [ParkingLot][Story7] feat: Super Parking Strategy

---
+++ src/main/java/com/parkinglot/SuperParkingStrategy.java
@@ -0,0 +1,14 @@
+package com.parkinglot;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class SuperParkingStrategy implements ParkingStrategy {
+    @Override
+    public ParkingLot selectParkingLot(List<ParkingLot> parkingLots) {
+        return parkingLots.stream()
+                .filter(ParkingLot::hasAvailablePosition)
+                .max(Comparator.comparingDouble(ParkingLot::getAvailablePositionRate))
+                .orElseThrow(NoAvailablePositionException::new);
+    }
+}
