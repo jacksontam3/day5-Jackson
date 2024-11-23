45c074f6425fd6665d80a4bf1504e2c68f49dcb6
Jackson Tam <Jackson.Tam@OOCL.com>
2024-11-23 12:50:12 +0800

    [ParkingLot][Story4] test, should_park_in_second_parking_lot_when_first_is_full

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -13,8 +13,8 @@ public class ParkingBoyTest {
     @Test
     void should_return_ticket_when_park_given_a_car_and_a_parking_lot(){
         //Given
-        ParkingLot parkingLot1 = new ParkingLot(1);
-        ParkingLot parkingLot2 = new ParkingLot(1);
+        ParkingLot parkingLot1 = new ParkingLot(10);
+        ParkingLot parkingLot2 = new ParkingLot(10);
 
         ParkingBoy parkingBoy = new ParkingBoy(Arrays.asList(parkingLot1, parkingLot2));
         Car car = new Car();
@@ -27,8 +27,8 @@ public class ParkingBoyTest {
     @Test
     void should_return_car_when_fetch_a_car_given_a_parking_lot() throws Exception {
         //Given
-        ParkingLot parkingLot1 = new ParkingLot(1);
-        ParkingLot parkingLot2 = new ParkingLot(1);
+        ParkingLot parkingLot1 = new ParkingLot(10);
+        ParkingLot parkingLot2 = new ParkingLot(10);
 
         ParkingBoy parkingBoy = new ParkingBoy(Arrays.asList(parkingLot1, parkingLot2));
         Car car = new Car();
@@ -42,8 +42,8 @@ public class ParkingBoyTest {
     @Test
     void should_return_two_cars_when_fetch_given_a_parking_lot() throws Exception {
         //Given
-        ParkingLot parkingLot1 = new ParkingLot(1);
-        ParkingLot parkingLot2 = new ParkingLot(1);
+        ParkingLot parkingLot1 = new ParkingLot(10);
+        ParkingLot parkingLot2 = new ParkingLot(10);
 
         ParkingBoy parkingBoy = new ParkingBoy(Arrays.asList(parkingLot1, parkingLot2));
         Car car = new Car();
@@ -61,8 +61,8 @@ public class ParkingBoyTest {
     @Test
     void should_return_error_message_when_fetch_given_wrong_parking_ticket() throws Exception {
         //Given
-        ParkingLot parkingLot1 = new ParkingLot(1);
-        ParkingLot parkingLot2 = new ParkingLot(1);
+        ParkingLot parkingLot1 = new ParkingLot(10);
+        ParkingLot parkingLot2 = new ParkingLot(10);
 
         ParkingBoy parkingBoy = new ParkingBoy(Arrays.asList(parkingLot1, parkingLot2));
         Car car = new Car();
@@ -77,8 +77,8 @@ public class ParkingBoyTest {
     @Test
     void should_return_nothing_when_fetch_given_used_parking_ticket() throws Exception {
         //Given
-        ParkingLot parkingLot1 = new ParkingLot(1);
-        ParkingLot parkingLot2 = new ParkingLot(1);
+        ParkingLot parkingLot1 = new ParkingLot(10);
+        ParkingLot parkingLot2 = new ParkingLot(10);
 
         ParkingBoy parkingBoy = new ParkingBoy(Arrays.asList(parkingLot1, parkingLot2));
         Car car = new Car();
@@ -110,21 +110,21 @@ public class ParkingBoyTest {
     @Test
     void should_park_in_second_parking_lot_when_first_is_full() throws Exception {
         // Given
-        ParkingLot parkingLot1 = new ParkingLot(1);
-        ParkingLot parkingLot2 = new ParkingLot(1);
+        ParkingLot parkingLot1 = new ParkingLot(10);
+        ParkingLot parkingLot2 = new ParkingLot(10);
         ParkingBoy parkingBoy = new ParkingBoy(Arrays.asList(parkingLot1, parkingLot2));
-        Car car1 = new Car();
-        Car car2 = new Car();
 
-        // When
-        Ticket ticket1 = parkingBoy.park(car1);
-        Ticket ticket2 = parkingBoy.park(car2);
+        for (int i = 0; i < 10; i++) {
+            parkingBoy.park(new Car());
+        }
+
+        //When
+        Car carInSecondLot = new Car();
+        Ticket ticketForSecondLot = parkingBoy.park(carInSecondLot);
 
         // Then
-        assertNotNull(ticket1);
-        assertNotNull(ticket2);
-        assertEquals(car1, parkingLot1.fetch(ticket1));
-        assertEquals(car2, parkingLot2.fetch(ticket2));
+        assertNotNull(ticketForSecondLot);
+        assertEquals(carInSecondLot, parkingBoy.fetch(ticketForSecondLot));
     }
 
 
